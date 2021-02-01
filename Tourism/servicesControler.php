8deb72be50fd1d5b2d50b3230027872d77c4c166
ushergodwin <65310616+ushergodwin@users.noreply.github.com>
2021-02-01 11:05:18 +0300

    Update servicesControler.php

--- Tourism/servicesControler.php
+++ Tourism/servicesControler.php
@@ -1,11 +1,19 @@
 <?php
-include 'DBConnection.php';
-class ToursimSevices
+include 'DatabaseConnection.php';
+class Toursim_Sevices
 {
   public function __construct()
   {
   }
   public function get_services() {
-    
+    $services = array();
+    $connection = DatabaseConnection::dbConnect();
+    $sql = "SELECT * FROM park-orders";
+    $result = mysqli_query($connection, $sql);
+    while($data = mysqli_fetch_assoc($result)){
+      $services[] = $data;
+    }
   }
+  return $services;
 }
+$services = new Toursim_Services();
