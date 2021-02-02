d6b9191259619bba6ac7c0d2c978e93b3bef1339
ushergodwin <65310616+ushergodwin@users.noreply.github.com>
2021-02-02 13:35:03 +0300

    Update app_view.php
    a static method called from serviceController

--- Tourism/app_view.php
+++ Tourism/app_view.php
@@ -5,3 +5,10 @@ $data = Tourism::get_services();
 
 //get the national parks
 $parks = Tourism::get_national_parks();
+//get packages
+$packages = Tourism::get_packages();
+//capture the approval action
+$response = "";
+if (isset($_REQUEST['approve'])) {
+    $response = Tourism::approve_order();
+}
