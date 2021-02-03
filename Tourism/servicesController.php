9777fe137abcc48c6169c79bfbfd2c71e90ac16e
ushergodwin <65310616+ushergodwin@users.noreply.github.com>
2021-02-03 18:00:24 +0300

    Update servicesController.php
    added the required methods and a User class
    Shifted the trait to DatabaseConnection.php

--- Tourism/servicesController.php
+++ Tourism/servicesController.php
@@ -1,25 +1,11 @@
 <?php
 include 'DatabaseConnection.php';
-trait getData
-{
-  public static function get($field) {
-    if (isset($_REQUEST[$field])):
-      return strip_tags($_GET[$field]);
-    else:
-      return $_GET;
-    endif;
-  }
-  public static function post($field) {
-    if (isset($_POST[$field])):
-      return strip_tags($_POST[$field]);
-    else:
-      return $_GET;
-    endif;
-  }
-}
 class Tourism
 {
   use getData;
+  public static $packageName;
+  public static $packageCost;
+  public static $packageId;
   public function __construct()
   {
   }
@@ -58,18 +44,6 @@ class Tourism
     }
     return $packages;
 
-  }
-  private static function failure(string $message = 'Oops, something went wrong, please try again later.') {
-    return '<div class="alert alert-warning alert-dismissible fade show">
-    <strong>Failure! </strong>'. $message.'
-    <button type="button" class="close" data-dismiss="alert">&times;</button>
-</div>';
-  }
-  private static function success(string $message) {
-    return '<div class="alert alert-success alert-dismissible fade show">
-    <strong>Success! </strong>'. $message.'
-    <button type="button" class="close" data-dismiss="alert">&times;</button>
-</div>';
   }
   public static function approve_order() {
     $connection = DatabaseConnection::dbConnect();
@@ -115,6 +89,82 @@ class Tourism
       return self::failure();
     }
   }
+  public static function get_package_by_id(){
+    $connection = DatabaseConnection::dbConnect();
+    $package_id = self::get('package');
+    $sql = "SELECT * FROM packages WHERE id = $package_id";
+    $result = mysqli_query($connection, $sql);
+    if(!empty($result)) {
+      while($data = mysqli_fetch_assoc($result)) {
+        self::$packageName = $data['name'];
+        self::$packageCost = $data['price'];
+        self::$packageId   = $data['id'];
+      }
+    }
+  }
+  public static function add_package() {
+    $connection = DatabaseConnection::dbConnect();
+    $name = self::post('packageName');
+    $price = self::post('packagePrice');
+    $sql = "INSERT INTO packages (name, price) VALUES ('$name', '$price')";
+    $checkpoint = "SELECT id FROM packages WHERE name = '$name'";
+    $check = mysqli_query($connection, $checkpoint);
+    if (mysqli_num_rows($check) > 0) {
+      $getID = mysqli_fetch_assoc($check);
+      $pkId = $getID['id'];
+      echo self::failure("Package name already exists. <br/> Please choose a different name or <a href='action-center?action=edit&package=$pkId' class='b-link font-weight-bold'>Rename</a> the existing package.");
+      return false;
+    }else {
+      mysqli_query($connection, $sql);
+      if(mysqli_affected_rows($connection) > 0) {
+        echo self::success("Package added successfully");
+      }else {
+        echo self::failure();
+      }
+    }
+  }
+  public static function num_of_packages() {
+    $connection = DatabaseConnection::dbConnect();
+    $sql = "SELECT COUNT(id) FROM packages";
+    $result = mysqli_query($connection, $sql);
+    $row = mysqli_fetch_assoc($result);
+    return $row['COUNT(id)']; 
+  }
+  public static function edit_package() {
+    $connection = DatabaseConnection::dbConnect();
+    $id = self::post('id');
+    $name = self::post('name');
+    $price = self::post('price');
+    $sql = "UPDATE packages SET price = $price, name = '$name' WHERE id = $id";
+    mysqli_query($connection, $sql);
+    if (mysqli_affected_rows($connection) > 0) {
+      echo self::success('Package Details Updated Succefully');
+    }else {
+      echo self::failure();
+    }
+  }
+  public static function delete_package() {
+    $connection = DatabaseConnection::dbConnect();
+    $id = self::post('id');
+    $sql = "UPDATE packages SET `status` = '1' WHERE id = $id";
+    mysqli_query($connection, $sql);
+    if (mysqli_affected_rows($connection) > 0) {
+      echo self::success("Package Deleted successfully! <a href='javascript:void(0)' class='btn btn-primary btn-sm' onclick='cancelDelete()' data-package='$id' id='cancel'>undo </a> ");
+    }else {
+      echo self::failure();
+    }
+  }
+  public static function undo_package_delete() {
+    $connection = DatabaseConnection::dbConnect();
+    $id = self::post('id');
+    $sql = "UPDATE packages SET status = '0' WHERE id = $id";
+    mysqli_query($connection, $sql);
+    if (mysqli_affected_rows($connection) > 0) {
+      echo self::success("Action undone successfully");
+    }else {
+      echo self::failure();
+    }
+  }
 }
 //undo order approval action
 if (isset($_POST['approve'])) {
@@ -123,4 +173,39 @@ if (isset($_POST['approve'])) {
 //undo cancle approval action
 if(isset($_POST['cancel'])) {
   echo Tourism::undo_cancel_approval();
+}
+//add package
+if(isset($_POST['action']) && $_POST['action'] == 'add') {
+  Tourism::add_package();
+}
+//update packages
+if(isset($_POST['action']) && $_POST['action'] == 'save') {
+  Tourism::edit_package();
+}
+//delete a package
+if(isset($_POST['action']) && $_POST['action'] == 'delete') {
+  Tourism::delete_package();
+}
+//undo the package delete action
+if(isset($_POST['action']) && $_POST['action'] == 'cancel') {
+  Tourism::undo_package_delete();
+}
+
+
+class User
+{
+  use getData;
+  public function __construct()
+  {
+    
+  }
+  public static function isLoggedIn() {
+    session_start();
+    if (!isset($_SESSION['username'])) {
+      self::redirect('index.php');
+    }
+  }
+  public static function userName() {
+    return $_SESSION['username'];
+  }
 }
